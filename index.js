b6fccb38ac031013d529e4a0813e47ac5654cd6c
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-13 19:48:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 updateObjectWithKeyAndValue(recipes, "prop2", "2")
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return recipes.key = "value";
+  return recipes["prop2"] = 2;
 }
 destructivelyUpdateObjectWithKeyAndValue(recipes, "prop2", 2)
