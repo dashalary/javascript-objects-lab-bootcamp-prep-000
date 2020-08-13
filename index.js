6a0d4398c558ae515d02e6ef930b639bc4ba0123
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-13 19:01:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {prop: 1};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, {prop2: 2});
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return recipes = {prop: 1, prop2: 2}
+}
