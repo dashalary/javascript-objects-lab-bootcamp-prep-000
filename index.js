37490823063ed3a4c219fa07221987bc26ef5031
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-13 20:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete recipes.prop;
-  return recipes;
+  delete object.key;
+  return object;
 }
