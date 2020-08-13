3fa8e8497b89584b09f67b83cf3c90271131ebb4
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-13 20:45:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj.key
- 
+  var newObj = Object.assign({}, recipes);
+  return delete newObj.prop;
 }
