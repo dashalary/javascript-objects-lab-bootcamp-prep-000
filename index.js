910f9026c857ebef79ca58560c059d2220c0c7b2
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-13 19:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {prop: 1};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, {prop2: 2}, {prop: 1}, {prop: "new value"});
+  return Object.assign({}, recipes, {prop2: 2});
 }
 
 
